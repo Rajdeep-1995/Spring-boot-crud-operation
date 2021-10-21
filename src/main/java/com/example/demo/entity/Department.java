065acf99235bccbdcb5a5e0e364babf924f06752
Rajdeep <rsondarva71@gmail.com>
2021-10-20 23:29:42 -0400

    Hibernate validation

--- src/main/java/com/example/demo/entity/Department.java
+++ src/main/java/com/example/demo/entity/Department.java
@@ -4,12 +4,26 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.*;
 
 @Entity
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long departmentId;
+    @NotBlank(message = "Please add department")
+    @Size(max = 20,min=2)
+    /*
+    @Email
+    @Positive
+    @Negative
+    @PositiveOrZero
+    @NegativeOrZero
+    @Future //date
+    @FutureOrPresent //date
+    @Past //date
+    @PastOrPresent //date
+    */
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
